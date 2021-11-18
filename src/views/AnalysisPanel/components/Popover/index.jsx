e479d3ddd481aa343c9a2a2771badf6b21e7ddb5
saltro <2373117747@qq.com>
2021-11-18 12:29:00 +0800

    style: 微调热力图样式

--- src/views/AnalysisPanel/components/Popover/index.jsx
+++ src/views/AnalysisPanel/components/Popover/index.jsx
@@ -9,8 +9,8 @@ export default function (props) {
       <div
         id="popover-container"
         style={{
-          top: top - 10 - 28 + 'px',
-          left: left - 40 + 'px',
+          top: top - 6 - 28 + 'px',
+          left: left - 30 + 'px',
           opacity: visibility ? 0.8 : 0,
         }}
       >
@@ -19,7 +19,7 @@ export default function (props) {
       <div
         id="triangle"
         style={{
-          top: top - 10 + 'px',
+          top: top - 6 + 'px',
           left: left - 6 + 'px',
           opacity: visibility ? 0.8 : 0,
         }}
