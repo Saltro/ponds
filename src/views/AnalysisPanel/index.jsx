eb6dd7a38f5a3bf01165c6ce0e0af8494b654fe2
saltro <2373117747@qq.com>
2021-11-18 09:18:36 +0800

    fix: 修改popover提示月份错误bug；修改热力图样式

--- src/views/AnalysisPanel/index.jsx
+++ src/views/AnalysisPanel/index.jsx
@@ -45,8 +45,8 @@ export default function () {
             values={heatmapValues}
             startDate={lastYearDate}
             endDate={curDate}
-            monthLabels={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
-            weekdayLabels={['日', '一', '二', '三', '四', '五', '六']}
+            monthLabels={['1 月', '2 月', '3 月', '4 月', '5 月', '6 月', '7 月', '8 月', '9 月', '10 月', '11 月', '12 月']}
+            weekdayLabels={['Sun', 'Mon', 'Tue', 'Wen', 'Thu', 'Fri', 'Sat']}
             showWeekdayLabels
             gutterSize={2}
             showOutOfRangeDays
@@ -54,7 +54,7 @@ export default function () {
               if (value === null) {
                 return 'color-rect color-empty';
               }
-              const colorMapIdx = [0, 5, 10, 15, 20, 25, 30].filter((item) => item <= value.count).length;
+              const colorMapIdx = [0, 5, 10, 15, 20, 25].filter((item) => item <= value.count).length;
               return 'color-rect ' + (colorMapIdx <= 0 ? 'color-empty' : `color-scale-${colorMapIdx}`);
             }}
             tooltipDataAttrs={({ date, count }) => {
@@ -62,7 +62,8 @@ export default function () {
                 return { 'data-tooltip': '没有记录' };
               }
               const d = new Date(date);
-              return { 'data-tooltip': `${d.getMonth()}月${d.getDate()}日 ${count}次记录` };
+              console.log(d);
+              return { 'data-tooltip': `${d.getMonth()+1}月${d.getDate()}日 ${count}次记录` };
             }}
             onMouseOver={(event, value) => {
               setPopoverVisibility(true);
